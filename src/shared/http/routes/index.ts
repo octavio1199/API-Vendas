0afec85bf8cfadf5da5ace0186548ca3df5c37bd
octavio1199 <octaviorocha1199@gmail.com>
2023-01-07 21:44:25 -0300

    Add ProfileRoutes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,5 +1,6 @@
 import productsRouter from '@modules/products/routes/products.routes';
 import passwordRouter from '@modules/users/routes/password.routes';
+import profileRouter from '@modules/users/routes/profile.routes';
 import sessionsRouter from '@modules/users/routes/sessions.routes';
 import usersRouter from '@modules/users/routes/users.routes';
 import { Router } from 'express';
@@ -10,9 +11,6 @@ routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
-
-routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello Dev' });
-});
+routes.use('/profile', profileRouter);
 
 export default routes;
