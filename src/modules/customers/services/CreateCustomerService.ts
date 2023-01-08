824c3cf972e3f6c0af57afa6cc0932043bafe6c8
octavio1199 <octaviorocha1199@gmail.com>
2023-01-07 22:21:34 -0300

    Customer - CRUD Services

---
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -0,0 +1,31 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import Customer from '../typeorm/entities/Customer';
+import { CustomersRepository } from '../typeorm/repositories/CustomersRepository';
+
+interface IRequest {
+  name: string;
+  email: string;
+}
+
+class CreateCustomerService {
+  public async execute({ name, email }: IRequest): Promise<Customer> {
+    const customersRepository = getCustomRepository(CustomersRepository);
+
+    const customerExists = await customersRepository.findByEmail(email);
+
+    if (customerExists) {
+      throw new AppError('There is already one customer with this email');
+    }
+
+    const customer = customersRepository.create({
+      name,
+      email,
+    });
+    await customersRepository.save(customer);
+
+    return customer;
+  }
+}
+
+export default CreateCustomerService;
