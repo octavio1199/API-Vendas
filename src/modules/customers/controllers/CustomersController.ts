85616b609d4919d7cee3c74f31cb072ebc2745f6
octavio1199 <octaviorocha1199@gmail.com>
2023-01-07 22:21:48 -0300

    Customer - Controller / Routes

---
+++ src/modules/customers/controllers/CustomersController.ts
@@ -0,0 +1,63 @@
+import { Request, Response } from 'express';
+import CreateCustomerService from '../services/CreateCustomerService';
+import DeleteCustomerService from '../services/DeleteCustomerService';
+import ListCustomerService from '../services/ListCustomerService';
+import ShowCustomerService from '../services/ShowCustomerService';
+import UpdateCustomerService from '../services/UpdateCustomerService';
+
+export default class CustomersController {
+  async index(req: Request, response: Response): Promise<Response> {
+    const listCustomers = new ListCustomerService();
+
+    const customers = await listCustomers.execute();
+    return response.json(customers).status(200);
+  }
+
+  async show(req: Request, response: Response): Promise<Response> {
+    const { id } = req.params;
+
+    const showCustomers = new ShowCustomerService();
+
+    const customer = await showCustomers.execute({ id });
+
+    return response.json(customer).status(200);
+  }
+
+  async create(req: Request, response: Response): Promise<Response> {
+    const { name, email } = req.body;
+
+    const createCustomer = new CreateCustomerService();
+
+    const customer = await createCustomer.execute({
+      name,
+      email,
+    });
+
+    return response.json(customer).status(201);
+  }
+
+  async update(req: Request, response: Response): Promise<Response> {
+    const { id } = req.params;
+    const { name, email } = req.body;
+
+    const updateCustomer = new UpdateCustomerService();
+
+    const customer = await updateCustomer.execute({
+      id,
+      name,
+      email,
+    });
+
+    return response.json(customer).status(200);
+  }
+
+  async delete(req: Request, response: Response): Promise<Response> {
+    const { id } = req.params;
+
+    const deleteCustomer = new DeleteCustomerService();
+
+    await deleteCustomer.execute({ id });
+
+    return response.status(204).send();
+  }
+}
