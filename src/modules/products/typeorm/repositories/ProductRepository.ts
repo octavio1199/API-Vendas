1365b73497b82e97e2dc4abc8f8e067a9391ac41
octavio1199 <octaviorocha1199@gmail.com>
2023-01-09 22:08:51 -0300

    Fix ProductRepository

--- src/modules/products/typeorm/repositories/ProductRepository.ts
+++ src/modules/products/typeorm/repositories/ProductRepository.ts
@@ -1,10 +1,24 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, In, Repository } from 'typeorm';
 import Product from '../entities/Product';
+
+interface IFindProducts {
+  id: string;
+}
 
 @EntityRepository(Product)
 export class ProductsRepository extends Repository<Product> {
   async findByName(name: string): Promise<Product | undefined> {
     const product = this.findOne({ where: { name } });
     return product;
+  }
+
+  async findAllByIds(products: IFindProducts[]): Promise<Product[]> {
+    const productIds = products.map(product => product.id);
+    const existentProducts = await this.find({
+      where: {
+        id: In(productIds),
+      },
+    });
+    return existentProducts;
   }
 }
